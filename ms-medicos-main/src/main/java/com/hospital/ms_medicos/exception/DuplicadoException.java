4fd6ae05119a393b0cc97014b5434428562b47cc
skaller118-lab <skaller118@gmail.com>
2026-06-02 23:22:09 -0400

    Add files via upload

---
+++ ms-medicos-main/src/main/java/com/hospital/ms_medicos/exception/DuplicadoException.java
@@ -0,0 +1,8 @@
+package com.hospital.ms_medicos.exception;
+
+public class DuplicadoException extends RuntimeException {
+    public DuplicadoException(String message) {
+        super(message);
+    
+    }
+}
