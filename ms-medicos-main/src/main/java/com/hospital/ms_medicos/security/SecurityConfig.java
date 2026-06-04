7df37d1f2ccdb85de03a93a18654ff190b5cfd31
JJ8s <164441267+JJ8s@users.noreply.github.com>
2026-06-03 23:30:24 -0400

    Add files via upload

---
+++ ms-medicos-main/src/main/java/com/hospital/ms_medicos/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.hospital.ms_medicos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public JwtAuthenticationFilter jwFilter(){
+        return new JwtAuthenticationFilter();
+    
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable())
+            .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth->auth
+                .requestMatchers("v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/medicos/**").hasAnyRole("ADMIN","MEDICO")
+                .requestMatchers(HttpMethod.POST, "/api/medicos/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/medicos/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/medicos/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwFilter(), UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+        }
+
+}
