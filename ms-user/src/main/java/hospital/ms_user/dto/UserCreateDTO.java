22f0a514e4d9d1a0be6be2b2e1e202199e4d7e4f
skaller118-lab <skaller118@gmail.com>
2026-06-28 00:53:17 -0400

    ms-user

---
+++ ms-user/src/main/java/hospital/ms_user/dto/UserCreateDTO.java
@@ -0,0 +1,32 @@
+package hospital.ms_user.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserCreateDTO {
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    private String apellido;
+
+    @NotBlank(message = "El número de teléfono es obligatorio")
+    @Size(min = 9, message = "Mínimo 9 dígitos")
+    @Column(unique = true, nullable = false)
+    private String telefono;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "Mínimo 8 caracteres")
+    private String contrasena;
+
+    @NotBlank(message = "El rol es obligatorio")
+    private String role; // Ejemplo: ADMIN, DOCTOR
+
+    public String generateSystemEmail() {
+        if (this.nombre == null || this.apellido == null) return null;
+        return (this.nombre + "." + this.apellido).toLowerCase().trim().replace(" ", ".") + "@hospital.com";
+    }
+}
