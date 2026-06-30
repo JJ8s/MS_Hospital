ad0ab4a3d7bec91e7c57e828600e1faf90dc9edb
skaller118-lab <skaller118@gmail.com>
2026-06-29 23:17:56 -0400

    Delete ms-user directory

--- ms-user/src/main/java/hospital/ms_user/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package hospital.ms_user.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String mensaje) {
-        super(mensaje);
-    }
-    
-}
