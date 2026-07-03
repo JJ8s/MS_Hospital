678adcf7aec4d07421f79ea01fbbd78c15d75cdd
skaller118-lab <skaller118@gmail.com>
2026-07-03 00:51:11 -0400

    Delete ms-user directory

--- ms-user/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT IGNORE INTO roles (id, name) VALUES (1, 'ROLE_ADMIN');
-INSERT IGNORE INTO roles (id, name) VALUES (2, 'ROLE_MEDICO');
-INSERT IGNORE INTO roles (id, name) VALUES (3, 'ROLE_USER');
