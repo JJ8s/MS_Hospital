665ea9c7b44b7be1a19f05e7a01c687ee2a8a7ae
skaller118-lab <skaller118@gmail.com>
2026-07-07 02:43:12 -0400

    ms-urgencia

---
+++ ms-urgencia/src/main/java/com/hospital/ms_urgencia/exception/NivelTriageInvalidoException.java
@@ -0,0 +1,8 @@
+package com.hospital.ms_urgencia.exception;
+
+public class NivelTriageInvalidoException extends RuntimeException {
+
+    public NivelTriageInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
