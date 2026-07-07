a8c36c5c27178d6f52a0697260673c8455d4fb19
skaller118-lab <skaller118@gmail.com>
2026-07-07 02:36:48 -0400

    Delete ms-pacientes-main directory

--- ms-pacientes-main/src/main/java/com/hospital/pacientes/exception/DuplicadoException.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.pacientes.exception;
-
-public class DuplicadoException extends RuntimeException{
-    public DuplicadoException(String message){
-        super(message);
-    }
-    
-}
