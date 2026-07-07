a4d3945a526a9f50d520f85413bee8df14204eed
skaller118-lab <skaller118@gmail.com>
2026-07-07 02:36:55 -0400

    Delete ms-recetas directory

--- ms-recetas/src/main/java/com/hospital/ms_recetas/exception/IntegracionInventarioException.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.ms_recetas.exception;
-
-public class IntegracionInventarioException extends RuntimeException {
-    public IntegracionInventarioException(String mensaje) {
-        super(mensaje);
-    }
-}
